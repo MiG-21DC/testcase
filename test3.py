f5ce14c81baa0b96472ba181c2a4c1c6cdaccc6a
Shawn <da_capo_usa@hotmail.com>
2018-03-08 16:46:49 -0500

    test

--- test3.py
+++ test3.py
@@ -346,7 +346,7 @@ def player_point(player_id):
     if res is None:
         return 404
     for item in res:
-        print(item.text)
+        print(str(item))
     return json.dumps({'success': 'true'})
 
 if __name__ == '__main__':
