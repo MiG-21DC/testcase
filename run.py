725e1258d62362620c6f645f90f1c9e6cea4944d
Shawn <da_capo_usa@hotmail.com>
2018-03-08 11:57:53 -0500

    test

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import json
 import uuid
 
 header = {'content-type':'application/json'}
-player1 = {'id': str(uuid.uuid1), 'nickname': 'Jason', 'email': 'Jason@gmail.com'}
-player2 = {'id': str(uuid.uuid1), 'nickname': 'Sushi', 'email': 'Random@outlook.com'}
+player1 = {'id': str(uuid.uuid1()), 'nickname': 'Jason', 'email': 'Jason@gmail.com'}
+player2 = {'id': str(uuid.uuid1()), 'nickname': 'Sushi', 'email': 'Random@outlook.com'}
 r = requests.post('http://0.0.0.0:5000/player', data=json.dumps(player1), headers=header)
 print(r.text)
 r = requests.put('http://0.0.0.0:5000/player', data=json.dumps(player2), headers=header)
