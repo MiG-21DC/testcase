63175a1ceb003e315a84a30228a23d36b6d0c4e6
Shawn <da_capo_usa@hotmail.com>
2018-03-07 23:07:57 -0500

    test

--- test3.py
+++ test3.py
@@ -6,54 +6,54 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://shawn:shawn@localhost/gamehive'
-# db = flask_sqlalchemy.SQLAlchemy(app)
-api = Api(app)
-
-# guild_stat = db.Table('guild_stat',
-#                       db.Column('player_id', db.String(128), db.ForeignKey('player.id')),
-#                       db.Column('guild_id', db.String(128), db.ForeignKey('guild.id'))
-#                       )
-#
-# item_stat = db.Table('item_stat',
-#                      db.Column('player_id', db.String(128), db.ForeignKey('player.id')),
-#                      db.Column('item_id', db.String(128), db.ForeignKey('item.id'))
-#                      )
-#
-#
-# class Player(db.Model):
-#     id = db.Column(db.String(128), primary_key=True)     #UUID
-#     nickname = db.Column(db.String(64), unique=True)
-#     email = db.Column(db.String(128), unique=True)
-#     guilds = db.relationship('Guild', secondary=guild_stat, back_populates='players')
-#     items = db.relationship('Item', secondary=item_stat, back_populates='owners')
-#
-#
-# class Guild(db.Model):
-#     id = db.Column(db.String(128), primary_key=True)        #UUID
-#     name = db.Column(db.String(64), unique=True)
-#     country_code = db.Column(db.String(16))
-#     players = db.relationship('Player', secondary=guild_stat, back_populates='guilds')
-#
-#
-# class Item(db.Model):
-#     id = db.Column(db.String(128), primary_key=True)
-#     name = db.Column(db.String(64), unique=True)
-#     owners = db.relationship('Player', secondary=item_stat, back_populates='items')
-#
-# db.create_all()
+db = flask_sqlalchemy.SQLAlchemy(app)
 
 
-class WorkOnPlayer(Resource):
-    def get(self, nickname):
-        print('here2')
-        res = db.session.query('Player').filter_by(nickname=nickname).first()
-        print('here')
-        print(res)
-        if res is None:
-            return 404
-        return json.dumps({'id': res.id, 'nickname': res.nickname, 'email': res.email})
+guild_stat = db.Table('guild_stat',
+                      db.Column('player_id', db.String(128), db.ForeignKey('player.id')),
+                      db.Column('guild_id', db.String(128), db.ForeignKey('guild.id'))
+                      )
 
-api.add_resource(WorkOnPlayer, '/player')
+item_stat = db.Table('item_stat',
+                     db.Column('player_id', db.String(128), db.ForeignKey('player.id')),
+                     db.Column('item_id', db.String(128), db.ForeignKey('item.id'))
+                     )
+
+
+class Player(db.Model):
+    id = db.Column(db.String(128), primary_key=True)     #UUID
+    nickname = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(128), unique=True)
+    guilds = db.relationship('Guild', secondary=guild_stat, back_populates='players')
+    items = db.relationship('Item', secondary=item_stat, back_populates='owners')
+
+
+class Guild(db.Model):
+    id = db.Column(db.String(128), primary_key=True)        #UUID
+    name = db.Column(db.String(64), unique=True)
+    country_code = db.Column(db.String(16))
+    players = db.relationship('Player', secondary=guild_stat, back_populates='guilds')
+
+
+class Item(db.Model):
+    id = db.Column(db.String(128), primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    owners = db.relationship('Player', secondary=item_stat, back_populates='items')
+
+db.create_all()
+
+
+# class WorkOnPlayer(Resource):
+#     def get(self, nickname):
+#         print('here2')
+#         res = db.session.query('Player').filter_by(nickname=nickname).first()
+#         print('here')
+#         print(res)
+#         if res is None:
+#             return 404
+#         return json.dumps({'id': res.id, 'nickname': res.nickname, 'email': res.email})
+# api = Api(app)
+# api.add_resource(WorkOnPlayer, '/player')
 
 # manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
 # player_blueprint = manager.create_api(Player, methods=['GET', 'POST', 'PUT', 'DELETE'])
@@ -64,6 +64,14 @@ api.add_resource(WorkOnPlayer, '/player')
 @app.route('/')
 def root():
     return 'Game Hive Player API'
+
+@app.route('/player', methods='GET'[])
+def get_all_players():
+    res = db.session.query('Player').all()
+    if res is None:
+        return 404
+    print(res)
+    return 0
 
 
 if __name__ == '__main__':
